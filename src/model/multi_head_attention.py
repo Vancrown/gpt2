72898929b6f8ede3b1ad98531a254b8e0d6f168c
Vancrown <lanxirye@outlook.com>
2026-09-10 22:08:14 -0400

    multi head attention

---
+++ src/model/multi_head_attention.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+from .causal_attention import CausalAttention
+
+
+class MultiHeadAttentionWrapper(nn.Module):
+    def __init__(
+        self,
+        d_in,
+        d_out,
+        context_length,
+        dropout,
+        num_heads,
+        qkv_bias=False,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.heads = nn.ModuleList(
+            [
+                CausalAttention(d_in, d_out, context_length, dropout, qkv_bias)
+                for i in range(num_heads)
+            ]
+        )
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
